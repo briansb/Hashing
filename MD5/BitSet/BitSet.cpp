2854a21ec4aa929c88a4ff6a0f66110ecec301b9
Brian Birmingham <brian.birmingham@odysseysr.com>
2022-03-16 13:48:14 -0500

    message

--- MD5/BitSet/BitSet.cpp
+++ MD5/BitSet/BitSet.cpp
@@ -16,20 +16,26 @@
 
 int main()
 {
-    std::string input_message = "They are deterministic";
-    //std::string input_message = "They";
     int number_of_blocks = 0;
     std::bitset<BITS_PER_WORD> message[MAX_BLOCKS][WORDS_PER_BLOCK];
 
+    // Get message from source code and load blocks
+    //std::string input_message = "They are deterministic";
+    //std::string input_message = "They";
+    std::string input_message = "They are deterministic.  They are non-deterministic.  They are ee";
     LoadMessageFromString(input_message, message, number_of_blocks);
+
+
 
     // need print function
     for (int i = 0; i < number_of_blocks; i++) {
+        std::cout << "Block " << i+1 << ":" << std::endl;
         for (int j = 0; j < WORDS_PER_BLOCK; j++) {
             std::cout << "Word " << std::setfill ('0') << std::setw(2) << j << " - ";
             std::cout << OutputWord_Binary(message, i, j) << " -> ";
             std::cout << OutputWord_Hex(message, i, j) << std::endl;
         }
+        std::cout << std::endl;
     }
     
     return 0;
--- MD5/BitSet/Input.cpp
+++ MD5/BitSet/Input.cpp
@@ -1,19 +1,22 @@
 
 #include "Input.h"
 
-
 void LoadMessageFromString(std::string in, std::bitset<BITS_PER_WORD> m[MAX_BLOCKS][WORDS_PER_BLOCK], int &block_number) {
-	//  Assumes only one block
+	
 	block_number = 1;
+	int block_length = BITS_PER_WORD * WORDS_PER_BLOCK;
+	int bit_counter = 1;
 	int word_index = 0;
 	int shift_amount = BITS_PER_WORD - BITS_PER_BYTE;
 	std::bitset< BITS_PER_WORD> single_entry;
 	for (size_t i = 0; i < in.length(); i++) {
+		if (bit_counter > block_length) { bit_counter = 1; word_index = 0; block_number++; }	 // filled up block
 
 		single_entry = char(in[i]);
 		single_entry = single_entry << shift_amount;
 		m[block_number-1][word_index] = m[block_number-1][word_index].to_ullong() + single_entry.to_ullong();
 		shift_amount -= BITS_PER_BYTE;
+		bit_counter += BITS_PER_BYTE;
 
 		if (shift_amount < 0){
 			shift_amount = BITS_PER_WORD - BITS_PER_BYTE;
@@ -21,9 +24,19 @@ void LoadMessageFromString(std::string in, std::bitset<BITS_PER_WORD> m[MAX_BLOC
 		}
 	}
 
+	//  insert 1
+	if (bit_counter > block_length) { bit_counter = 1; word_index = 0; block_number++; }
+	std::cout << "Block number = " << block_number << ", word index = " << word_index << ", shift_amount = " << shift_amount << ", bit_counter = " << bit_counter << std::endl;
 
-	// need to compute remaining bits...to determine if another block is needed
+	single_entry = 128;
+	single_entry = single_entry << shift_amount;
+	m[block_number-1][word_index] = m[block_number-1][word_index].to_ullong() + single_entry.to_ullong();
+	bit_counter++;
+
+	// how much padding do we need?  We need at least 64 bits for the size
 	
+
+
 
 	return;
 }
