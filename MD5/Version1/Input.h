853c7e42f8a93db07d96802c9702ac24f4a34ab8
Brian Birmingham <brian.birmingham@odysseysr.com>
2022-03-23 09:21:28 -0500

    updates

---
+++ MD5/Version1/Input.h
@@ -0,0 +1,13 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+
+
+
+
+
+
+
+
+
+#endif
--- MD5/Version1/Version1.cpp
+++ MD5/Version1/Version1.cpp
@@ -5,6 +5,8 @@
 #include <sstream>
 #include <iomanip>
 
+#include "Input.h"
+
 template< typename T >
 std::string int_to_hex(T i)
 {
@@ -26,30 +28,7 @@ std::string int_to_binary(T i)
 
 int main()
 {
-    uint8_t  a8;
-    uint16_t a16;
-    uint32_t a32;
-    uint64_t a64;
-
-    a8 = 17;
-    a16 = 19;
-    a32 = 21;
-    a64 = 23;
-
-    //std::cout << "a8 = " << int_to_binary(a8) << std::endl;
-    //std::cout << "a16 = " << int_to_binary(a16) << std::endl;
-    //std::cout << "a32= " << int_to_binary(a32) << std::endl;
-    //std::cout << "a64 = " << int_to_binary(a64) << std::endl;
-    //std::cout << std::endl;
-
-    //
-    //std::cout << "a8 = " << int_to_hex(a8) << std::endl;
-    //std::cout << "a16 = " << int_to_hex(a16) << std::endl;
-    //std::cout << "a32 = " << int_to_hex(a32) << std::endl;
-    //std::cout << "a64 = " << int_to_hex(a64) << std::endl;
-    //std::cout << std::endl;
-
-
+ 
     //  need spacing between bytes
     //  a block is 16 32-bit words
     //  use a vector for the collection of blocks
@@ -57,17 +36,31 @@ int main()
     std::string message_string = "Brian Birmingham";
     uint32_t single_character;
     uint32_t word = 0;
+    uint32_t block[16];
     int shift_amount = 24;
-    for (int i = 0; i < 4; i++) {
+    int word_index = 0;
+    for (size_t i = 0; i < message_string.length(); i++) {
         single_character = char(message_string[i]);
         single_character = single_character << shift_amount;
         word = word + single_character;
         shift_amount -= 8;
+
+        if (shift_amount < 0) {
+            shift_amount = 24;
+            block[word_index] = word;
+            word = 0;
+            word_index++;
+        }
+
     }
 
 
-    std::cout << "word= " << int_to_binary(word) << std::endl;
-    std::cout << "word = " << int_to_hex(word) << std::endl;
+    std::cout << "word= " << int_to_binary(block[0]) << std::endl;
+    std::cout << "word = " << int_to_hex(block[0]) << std::endl;
+    std::cout << "word= " << int_to_binary(block[1]) << std::endl;
+    std::cout << "word = " << int_to_hex(block[1]) << std::endl;
+    std::cout << "word= " << int_to_binary(block[2]) << std::endl;
+    std::cout << "word = " << int_to_hex(block[2]) << std::endl;
 
     return 0;
 }
