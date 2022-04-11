d9e3e89d5686bb6e9dc27502d3b3974792422449
Brian Birmingham <brian.birmingham@odysseysr.com>
2022-04-11 12:59:57 -0500

    updates

--- MD5/Version5/Version5.cpp
+++ MD5/Version5/Version5.cpp
@@ -29,19 +29,20 @@ int main()
     std::vector<uint32_t> words;
     uint8_t digest[8];  // was 16
     while (counter < 10) {
-        words.clear();
-        //  message string should be less than 24 characters
-        message_string = message_base + std::to_string(counter);
-        std::cout << "Message(" << message_string << ") = " << OutputHex(message_string) << std::endl;
-        LoadStringMessageIntoMessageVector(message_string, words);
+      words.clear();
+      //  message string should be <= 27 characters
+      message_string = message_base + std::to_string(counter);
+      std::cout << "Message(" << message_string << ") = " << OutputHex(message_string) << std::endl;
+      LoadStringMessageIntoMessageVector(message_string, words);
+      //for (size_t i = 0; i<words.size(); i++) { std::cout << OutputHex(words[i]) << " "; } std::cout << std::endl;
 
-        GetMD5Hash(words, digest);
+      GetMD5Hash(words, digest);
 
-        std::cout << "   Hash(" << message_string << ") = ";
-        for (int i = 0; i < 8; i++) { std::cout << std::setfill('0') << std::setw(2) << std::hex << +digest[i]; }
-        std::cout << std::endl << std::endl;
+      std::cout << "   Hash(" << message_string << ") = ";
+      for (int i = 0; i < 8; i++) { std::cout << std::setfill('0') << std::setw(2) << std::hex << +digest[i]; }
+      std::cout << std::endl << std::endl;
 
-        counter++;
+      counter++;
     }
     return 0;
 }
